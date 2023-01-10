5b7ab52116fc0938faf15a11f42972c5aa0b862b
vladimir <java_lessons@rambler.ru>
2023-01-10 20:12:11 +0300

    first commit

---
+++ src/Calculator.java
@@ -0,0 +1,17 @@
+public class Calculator {
+
+    //метод для сложения типов int
+    public int addition(int a, int b) {
+        return a + b;
+    }
+
+    //метод для вычитания типов int
+    public int subtraction(int a, int b) {
+        return a - b;
+    }
+
+    /*
+
+     */
+
+}
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        System.out.println(calculator.addition(2, 2));
+        System.out.println(calculator.subtraction(6, 5));
+
+    }
+}
